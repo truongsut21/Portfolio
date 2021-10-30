027b876b530230d356508c747cb528e3316c0b8a
truongsut21 <dotruong0704@gmail.com>
2021-10-30 10:35:49 +0700

    n6_portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,6 +137,20 @@ modalBtns.forEach((modalBtn, i) => {
   })
 })
 
+// =================== PORTFOLIO ================
+let swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
 
 
 
